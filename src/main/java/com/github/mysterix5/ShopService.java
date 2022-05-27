42149425dde2dd6fc6567b84f330a52f589da562
Lukas <lukasduechting@mailbox.org>
2022-05-27 12:36:41 +0200

    add shop service class with tests

---
+++ src/main/java/com/github/mysterix5/ShopService.java
@@ -0,0 +1,36 @@
+package com.github.mysterix5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShopService {
+    private final ProductRepo productRepo;
+    private final OrderRepo orderRepo;
+
+    public ShopService(ProductRepo productRepo, OrderRepo orderRepo){
+        this.productRepo = productRepo;
+        this.orderRepo = orderRepo;
+    }
+
+    public Product getProduct(String productId){
+
+        return productRepo.get(productId);
+    }
+
+    public List<Product> listProducts(){
+        return productRepo.list();
+    }
+
+    public void addOrder(List<String> productIds){
+        orderRepo.add(productRepo, productIds);
+    }
+
+    public Order getOrder(String orderId){
+        return orderRepo.get(orderId);
+    }
+
+    public List<Order> listOrders(){
+
+        return orderRepo.list();
+    }
+}
